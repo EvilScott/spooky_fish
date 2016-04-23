83d8cf2a098cf76e8879d27b4d86130ff6377f59
Brian Towle <thebriantowle@gmail.com>
2016-04-23 15:31:00 -0700

    seeds for net_types, action_types, regions, reasons

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,4 +43,146 @@ sources = [
     }
 ]
 
+reasons = [
+    {
+        reason_type: 'New'
+    },
+    {
+        reason_type: 'Replace Recycled'
+    },
+    {
+        reason_type: 'Replace Lost(Stolen)'
+    },
+    {
+        reason_type: 'Replace Lost(Weather)'
+    },
+    {
+        reason_type: 'Replace Lost(Damaged)'
+    },
+    {
+        reason_type: 'Replace Lost(Abandoned)'
+    },
+    {
+        reason_type: 'Replace Lost(Unknown)'
+    }
+]
+
+regions = [
+    {
+        region_name: 'Ilwaco'
+    },
+    {
+        region_name: 'Westport-Ocean Shores'
+    },
+    {
+        region_name: 'Willapa Bay'
+    },
+    {
+        region_name: 'Grays Harbor'
+    },
+    {
+        region_name: 'Lapush'
+    },
+    {
+        region_name: 'Neah Bay'
+    },
+    {
+        region_name: 'Sekiu and Pillar Point'
+    },
+    {
+        region_name: 'East Juan de Fuca Strait'
+    },
+    {
+        region_name: 'San Juan Islands'
+    },
+    {
+        region_name: 'Deception Pass, Hope Island, Skagit Bay'
+    },
+    {
+        region_name: 'Ports Susan and Gardner'
+    },
+    {
+        region_name: 'Admiralty Inlet'
+    },
+    {
+        region_name: 'Seattle-Bremerton Area'
+    },
+    {
+        region_name: 'Tacoma-Vashon Island'
+    },
+    {
+        region_name: 'Hood Canal'
+    },
+    {
+        region_name: 'South Puget Sound'
+    }
+]
+
+action_types = [
+    {
+        action_name: 'Sold'
+    },
+    {
+        action_name: 'Recycled'
+    },
+    {
+        action_name: 'Found'
+    }
+]
+
+net_types = [
+    {
+        net_code: 'WT06',
+        mesh_size: 6.2,
+        twine_size: 3.0,
+        strands: 2,
+        net_construction: 'Layered',
+        twine_type: 'Twisted',
+        twine_material: 'Natural',
+        net_use: 'Trawl',
+        origin: 'Taiwan',
+        color: 'White'
+    },
+    {
+        net_code: 'BL01',
+        mesh_size: 3.8,
+        twine_size: 2.3,
+        strands: 3,
+        net_construction: 'Knotted',
+        twine_type: 'Twisted',
+        twine_material: 'Natural',
+        net_use: 'Trawl',
+        origin: 'Indonesia',
+        color: 'Blue'
+    },
+    {
+      net_code: 'BK03',
+      mesh_size: 25.0,
+      twine_size: 2.0,
+      strands: 2,
+      net_construction: 'Knotted',
+      twine_type: 'Braided',
+      twine_material: 'Natural',
+      net_use: 'Gill Net',
+      origin: 'Indonesia',
+      color: 'Black'
+    },
+    {
+        net_code: 'GR71',
+        mesh_size: 350.0,
+        twine_size: 4.0,
+        strands: 3,
+        net_construction: 'Knotted',
+        twine_type: 'Twisted',
+        twine_material: 'Synthetic',
+        net_use: 'Trawl',
+        origin: 'Taiwan',
+        color: 'Green'
+    },
+]
+
 sources.each { |source| Source.create(source) }
+reasons.each { |reason| Reason.create(reason) }
+regions.each { |region| Region.create(region) }
+action_types.each{ |action_type| ActionType.create(action_type)}
+net_types.each{ |net_type| NetType.create(net_type)}
