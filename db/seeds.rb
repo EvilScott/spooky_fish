25fda2c8cb59dd86b751f60b75ccce439258b535
Robert Reis <RobertR@vulcan.com>
2016-04-23 12:16:43 -0700

    seed for sources

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+types = {
+    data: SourceType.create(name: 'Data'),
+    paper: SourceType.create(name: 'Paper'),
+    website: SourceType.create(name: 'Website'),
+}
+
+sources = [
+    { name: 'NOAA Marine Debris Program',
+      source_type: types[:website],
+      description: 'The NOAA Marine Debris Program offers several nationwide, competitive funding opportunities for marine debris projects.',
+      url: 'https://marinedebris.noaa.gov/'
+    },
+]
+
+sources.each { |source| Source.create(source) }
