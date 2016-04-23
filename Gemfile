30fa2011b89d1a69968cfa7ae1ff73b56094f202
Brian Towle <thebriantowle@gmail.com>
2016-04-23 16:42:11 -0700

    added Credit#to_qr and Credit::generate

--- Gemfile
+++ Gemfile
@@ -19,6 +19,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # bootstrap
 gem 'twitter-bootstrap-rails'
 
+# png QR Code generation
+gem 'rqrcode_png', '~> 0.1.5'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/models/credit.rb
+++ app/models/credit.rb
@@ -3,4 +3,15 @@ class Credit < ActiveRecord::Base
   def to_s
     credit_code
   end
+
+  #view usage: <img src="<%= some_credit.to_qr (width, height)%>" />
+  def to_qr(width = 200, height = 200)
+    qr = RQRCode::QRCode.new( 'someString', :size => 5, :level => :h )
+    png = qr.to_img.resize(width,height)
+    png.to_data_url
+  end
+
+  def self.generate
+    create(credit_code: SecureRandom.uuid)
+  end
 end
