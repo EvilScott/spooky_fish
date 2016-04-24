8461d6591e78b5783b1d76c73a64a5ecc86e35f6
Robert Reis <RobertR@vulcan.com>
2016-04-23 17:19:08 -0700

    fix the form and add validations

--- app/controllers/gear_controller.rb
+++ app/controllers/gear_controller.rb
@@ -3,16 +3,26 @@ class GearController < ApplicationController
   end
 
   def recycle
-    @recycle = GearAction.new
+    @recycle = GearAction.new(action_type: ActionType.find_by_action_name('Recycled'))
   end
 
   def sold
-    @purchase = GearAction.new(action_type_id: 1) # TODO placeholder
-    @regions = Region.all
-    @reasons = Reason.all
-    @net_types = NetType.all
+    @sold = GearAction.new(action_type: ActionType.find_by_action_name('Sold'))
   end
 
   def submit
+    new_action = GearAction.create(gear_action_params)
+    if new_action.errors.empty?
+      flash[:notice] = 'Report generated successfully'
+    else
+      flash[:error] = new_action.errors.full_messages
+    end
+    redirect_to sold_path
+  end
+
+  private
+  def gear_action_params
+    params.require(:gear_action).permit(:action_type_id, :owner, :net_type_id, :region_id, :reason_id,
+                                        :recycling_credit_code, :disposal_receipt)
   end
 end
--- app/models/gear_action.rb
+++ app/models/gear_action.rb
@@ -4,4 +4,20 @@ class GearAction < ActiveRecord::Base
   belongs_to :region
   belongs_to :reason
   belongs_to :credit
+
+  validates :owner, presence: true
+  validates :credit, absense: true, unless: :expect_credit?
+  validates :disposal_receipt, absense: true, unless: :expect_disposal?
+
+  def is_sold?
+    action_type == ActionType.find_by_action_name('Sold')
+  end
+
+  def expect_credit?
+     is_sold? && reason == Reason.find_by_reason_type('Replace Recycled')
+  end
+
+  def expect_disposal?
+    is_sold? && reason == Reason.find_by_reason_type('Replace Disposed')
+  end
 end
