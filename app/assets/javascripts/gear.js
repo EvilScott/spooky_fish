f8735edd391390d1ed0094a15fbe12dae121546b
Robert Reis <RobertR@vulcan.com>
2016-04-23 20:01:30 -0700

    make the gear form better

--- app/assets/javascripts/gear.js
+++ app/assets/javascripts/gear.js
@@ -1,4 +1,28 @@
 $(function() {
     var $newGearForm = $('#new_gear_action');
-
-})();
+    $newGearForm.find('#gear_action_reason_id').change(function() {
+        var reasonVal = parseInt($(this).val());
+        $newGearForm
+            .find('#recycling_credit_code_section, #disposal_receipt_section')
+                .find('input')
+                .attr('disabled', 'disabled')
+                .end()
+            .hide();
+        if (reasonVal === 2) {
+            $newGearForm
+                .find('#recycling_credit_code_section')
+                    .find('input')
+                    .removeAttr('disabled')
+                    .end()
+                .show();
+        } else if (reasonVal === 3) {
+            $newGearForm
+                .find('#disposal_receipt_section')
+                    .find('input')
+                    .removeAttr('disabled')
+                    .end()
+                .removeAttr('disabled')
+                .show();
+        }
+    });
+});
